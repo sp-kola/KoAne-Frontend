6cfbc607717e10faad1ee1c604266a1992802285
Oshara Amarasiriwardena <amanthi1995@gmail.com>
2020-07-04 14:59:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
-import S1 from './Screen1';
-import S2 from './Screen2';
 import LoginRegister from './src/LoginRegister';
 import Select_option from './src/Select_option';
 import Login from './src/Login';
