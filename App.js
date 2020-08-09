1887eeca1b3fd1c20b12177dd97427251a8401a9
Oshara Amarasiriwardena <amanthi1995@gmail.com>
2020-08-09 21:27:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -17,44 +17,24 @@ import Testing from './src/screens/Testing';
 import Home from './src/screens/HomePage/Home';
 import AdminHome from './src/screens/AdminHome';
 import viewProduct from './src/components/Product/viewProduct';
+import SplashScreen from './src/SplashScreen';
 
 const RootStack = createStackNavigator();
 
 function App() {
   return (
+    <NavigationContainer>
+      <RootStack.Navigator headerMode="none">
+        <RootStack.Screen name="SplashScreen" component={SplashScreen} />
 
-    <NavigationContainer>     
-      <RootStack.Navigator headerMode='none' >
-      
-      <RootStack.Screen
-          name="LoginRegister"
-          component={LoginRegister}
-        /> 
+        <RootStack.Screen name="LoginRegister" component={LoginRegister} />
         <RootStack.Screen name="Addnewproduct" component={addProduct} />
-      <RootStack.Screen
-        name="VenderSearch"
-        component= {VenderSearch}
-          />
-      <RootStack.Screen
-        name="CustomerHome"
-        component= {CustomerHome}
-          />
-      <RootStack.Screen
-        name="VendorHome"
-        component= {VendorHome}
-          />
-      <RootStack.Screen
-        name="AdminHome"
-        component= {AdminHome}
-          />
-        <RootStack.Screen
-          name="Select_option"
-          component={Select_option}
-        />
-        <RootStack.Screen
-          name="Login"
-          component={Login}
-        />
+        <RootStack.Screen name="VenderSearch" component={VenderSearch} />
+        <RootStack.Screen name="CustomerHome" component={CustomerHome} />
+        <RootStack.Screen name="VendorHome" component={VendorHome} />
+        <RootStack.Screen name="AdminHome" component={AdminHome} />
+        <RootStack.Screen name="Select_option" component={Select_option} />
+        <RootStack.Screen name="Login" component={Login} />
         <RootStack.Screen
           name="CustomerRegister"
           component={CustomerRegister}
