65a390247fac521bc6ff0d381f3f8175c5b216fb
Oshara Amarasiriwardena <amanthi1995@gmail.com>
2020-08-09 21:26:26 +0530

    Update auth.js

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,63 +1,59 @@
 // import {TRY_AUTH, AUTH_SET_TOKEN} from './actionType'
-import { uiStartLoading,uiStopLoading } from './index'
+import {uiStartLoading, uiStopLoading} from './index';
 
+export const login = (authData, nav) => {
+  return dispatch => {
+    dispatch(uiStartLoading());
+    //console.log('in login')
+    let url = 'http://192.168.1.6:3300/user/login';
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(prasedRes => {
+        dispatch(uiStopLoading());
+        console.log(prasedRes.type);
+        const type = prasedRes.type;
+        if (type === 'Admin') {
+          nav.navigation.push('AdminHome', {
+            user: prasedRes,
+          });
+        } else if (type === 'Vendor') {
+          nav.navigation.push('VendorHome', {
+            user: prasedRes,
+          });
+        } else {
+          nav.navigation.push('CustomerHome', {
+            user: prasedRes,
+          });
+        }
 
-export const login = (authData,nav) => {
-    return dispatch => {
-        dispatch(uiStartLoading());
-        //console.log('in login')
-        let url = 'http://192.168.8.162:3300/user/login'
-        fetch(url,{
-            method: "POST",
-            body: JSON.stringify({
-                email: authData.email,
-                password: authData.password,
-                returnSecureToken: true
-            }),
-            headers: {
-                "Content-Type" : "application/json"
-            }
-        })
-        .then(res => {return res.json()})
-        .then(prasedRes => {
-            dispatch(uiStopLoading())
-            console.log(prasedRes.type)
-            const type = prasedRes.type
-            if(type === 'Admin'){
-                nav.navigation.push('AdminHome',{
-                    user: prasedRes
-                })
-            }
-            else if(type === 'Vendor'){
-                nav.navigation.push('VendorHome',{
-                    user: prasedRes
-                })
-            }
-            else {
-                nav.navigation.push('CustomerHome',{
-                    user: prasedRes
-                })
-            }
-            
-            // if(!prasedRes.idToken){
-            //     alert("Authentication failed! Please try again ")
-                
-            // }
-            // else{   
-            //     dispatch(authSetToken(prasedRes.idToken))
-            //     nav.navigation.push('SideDrawer')
-            // }
-            
-        })
-        .catch(err => {
-            dispatch(uiStopLoading())
-            console.log(err)
-            alert('Authentication failed! Please try again ')
-        })
-    }
-}
+        // if(!prasedRes.idToken){
+        //     alert("Authentication failed! Please try again ")
 
-
+        // }
+        // else{
+        //     dispatch(authSetToken(prasedRes.idToken))
+        //     nav.navigation.push('SideDrawer')
+        // }
+      })
+      .catch(err => {
+        dispatch(uiStopLoading());
+        console.log(err);
+        alert('Authentication failed! Please try again ');
+      });
+  };
+};
 
 // export const authSetToken = token => {
 //     return {
@@ -78,6 +74,6 @@ export const login = (authData,nav) => {
 //             }
 //         })
 //         return promise
-        
+
 //     }
 // }
