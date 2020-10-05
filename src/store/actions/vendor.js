8cab703d5b3ef3f0f4a556839ec053ff7fa1ea43
Oshara Amarasiriwardena <amanthi1995@gmail.com>
2020-10-05 12:07:57 +0530

    commit 1

--- src/store/actions/vendor.js
+++ src/store/actions/vendor.js
@@ -1,25 +1,23 @@
-export const vendorSignup = (signupData,nav) => {
-    return dispatch => {
-        //console.log(locationData);
-        fetch('http://192.168.8.162:3300/vendor/signup',{
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-              },
-            body: JSON.stringify(signupData)
-            
-        })
-        .catch(err => console.log(err))
-        .then(res => {
-            return res.json()
-            
-        })
-        .then(parsedRes => {
-            console.log(parsedRes)
-            nav.navigation.push('VendorHome',{
-                user: parsedRes
-            })
-        })
-    }  
-}
+export const vendorSignup = (signupData, nav) => {
+  return dispatch => {
+    //console.log(locationData);
+    fetch('http://192.168.1.6:3300/vendor/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(signupData),
+    })
+      .catch(err => console.log(err))
+      .then(res => {
+        return res.json();
+      })
+      .then(parsedRes => {
+        console.log(parsedRes);
+        nav.navigation.push('VendorHome', {
+          user: parsedRes,
+        });
+      });
+  };
+};
