08884554479ea1dddd8862bbc2755398d28d87a7
Sulakshana Ranawake <sula.ranawake@gmail.com>
2020-10-12 23:27:13 +0530

    merge conflicts solved

--- src/components/Product/addProduct.js
+++ src/components/Product/addProduct.js
@@ -60,7 +60,6 @@ export default class addProduct extends Component {
       return fetch('http://192.168.1.101:3300/product/create', {
         method: 'POST',
         headers: {
-          // Accept: 'application/json',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
